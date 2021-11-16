480769216e7157804c1976179450bfa3d28e4f6f
904154001 <904154001@L829WD150074.edu.cbe.ab.ca>
2021-11-16 11:33:42 -0700

    idk

--- RecursiveSorter/RecursiveSorter/src/RecursiveSorter.java
+++ RecursiveSorter/RecursiveSorter/src/RecursiveSorter.java
@@ -1,6 +1,36 @@
 public class RecursiveSorter {
 	
 	public void doQuickSort(IntegerArray clone) {
+		int low = 0;
+		int high = clone.length();
+		
+		 if (low < high)
+		    {
+		        pi = partition(clone, low, high);
+
+		        doQuickSort(clone, low, pi - 1);  // Before pi
+		        doQuickSort(clone, pi + 1, high); // After pi
+		    }
+	}
+
+	private void partition(IntegerArray clone, int low, int high) {
+		
+		 int pivot = clone.read(high);  
+		 
+		    int i = (low - 1) 
+
+		    for (j = low; j <= high- 1; j++)
+		    {
+		        // If current element is smaller than the pivot
+		        if (clone.read(j) < pivot)
+		        {
+		            i++;    // increment index of smaller element
+		            swap arr[i] and arr[j]
+		        }
+		    }
+		    swap arr[i + 1] and arr[high])
+		    return (i + 1)
+		}
 	}
 
 	public void doMergeSort(IntegerArray array) {
