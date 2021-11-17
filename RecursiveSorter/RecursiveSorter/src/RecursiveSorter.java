9802d7123b9afb729bedb2af32a97b7b21d2b8a3
904154001 <904154001@L829WD150074.edu.cbe.ab.ca>
2021-11-17 09:41:00 -0700

    swap method created

--- RecursiveSorter/RecursiveSorter/src/RecursiveSorter.java
+++ RecursiveSorter/RecursiveSorter/src/RecursiveSorter.java
@@ -1,30 +1,44 @@
+
 public class RecursiveSorter {
+	 public static void swap(IntegerArray numbers, int IndexA, int IndexB){
+	        int numbersA = numbers.read(IndexA);
+	        numbers.write(IndexA, numbers.read(IndexB));;
+	        numbers.write(IndexB, numbersA);
+	    }
+	private void partition(IntegerArray clone, int low, int high) {
+		
+		 int pivot = clone.read(high);  
+		 
+		    int i = (low - 1); 
+
+		    for (j = low; j <= high- 1; j++)
+		    {
+		        if (clone.read(j) < pivot)
+		        {
+		            i++; 
+		            swap arr[i] and arr[j]
+		        }
+		    }
+		    swap arr[i + 1] and arr[high])
+		    return (i + 1)
+		}
+	}
+
 	
 	public void doQuickSort(IntegerArray clone) {
-		int low = 0;
-		int high = clone.length();
-		
-		 if (low < high)
-		    {
-		        pi = partition(clone, low, high);
-
-		        doQuickSort(clone, low, pi - 1);  // Before pi
-		        doQuickSort(clone, pi + 1, high); // After pi
-		    }
 	}
 
 	private void partition(IntegerArray clone, int low, int high) {
 		
 		 int pivot = clone.read(high);  
 		 
-		    int i = (low - 1) 
+		    int i = (low - 1); 
 
 		    for (j = low; j <= high- 1; j++)
 		    {
-		        // If current element is smaller than the pivot
 		        if (clone.read(j) < pivot)
 		        {
-		            i++;    // increment index of smaller element
+		            i++; 
 		            swap arr[i] and arr[j]
 		        }
 		    }
