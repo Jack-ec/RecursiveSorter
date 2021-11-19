f72e9058f53667b0a5181c0e4bf8641614091ad7
904154001 <904154001@L829WD150074.edu.cbe.ab.ca>
2021-11-19 09:27:31 -0700

    done

--- RecursiveSorter/RecursiveSorter/src/RecursiveSorter.java
+++ RecursiveSorter/RecursiveSorter/src/RecursiveSorter.java
@@ -1,4 +1,3 @@
-
 public class RecursiveSorter {
 	public static void swap(IntegerArray numbers, int IndexA, int IndexB){
 		int numbersA = numbers.read(IndexA);
@@ -37,10 +36,61 @@ public class RecursiveSorter {
 	public void doQuickSort(IntegerArray clone) {
 		quickSort(clone, 0, clone.length() - 1);
 	}
+	
+	 void merge(IntegerArray array, int l, int m, int r)
+	    {
 
+	        int n1 = m - l + 1;
+	        int n2 = r - m;
+	  
+	        int L[] = new int[n1];
+	        int R[] = new int[n2];
+	  
+	        for (int i = 0; i < n1; ++i)
+	            L[i] = array.read(l + i);
+	        for (int j = 0; j < n2; ++j)
+	            R[j] = array.read(m + 1 + j);
+	        int i = 0, j = 0;
+	        int k = l;
+	        
+	        while (i < n1 && j < n2) {
+	            if (L[i] <= R[j]) {
+	                array.write(k, L[i]);
+	                i++;
+	            }
+	            else {
+	                array.write(k, R[j]);
+	                j++;
+	            }
+	            k++;
+	        }
+	        while (i < n1) {
+	            array.write(k, L[i]);;
+	            i++;
+	            k++;
+	        }
 
+	        while (j < n2) {
+	            array.write(k, R[j]);
+	            j++;
+	            k++;
+	        }
+	    }
+	 void mergesort(IntegerArray array, int l, int r)
+	    {
+	        if (l < r) {
+
+	            int m =l+ (r-l)/2;
+	  
+	            mergesort(array, l, m);
+	            mergesort(array, m + 1, r);
+	  
+	            merge(array, l, m, r);
+	        }
+	    }
 
 	public void doMergeSort(IntegerArray array) {
+		quickSort(array, 0, array.length() - 1);
 	}
 
 }
